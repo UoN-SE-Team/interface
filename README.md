f355a0681cdbf4fde2b9fdb9f93d32cb6e581811
Andy Palmer <alpsquid@gmail.com>
2014-12-02 15:38:16 +0000

    Base structure and project setup complete, I think...

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 interface
 =========
+
+How to Contribute: https://docs.google.com/document/d/1uZuK7mYnSa-T8RPH2uDSDl7DX1dbAuUPPqKx8MbAxx4
---
+++ client/src/scripts/main.js
@@ -0,0 +1,30 @@
+
+// Loads page content, sets the title and changes the active navbar button
+function loadPage(id, title, file) {
+	// In the final implementation, we'll be using a full templating framework so SEO won't be a problem
+	if (title != document.title) {
+		$(id).load(file);
+		document.title = title + " - " + document.title.split(" - ")[1];
+		switchHeader(title);
+	}
+}
+
+// Loads the navbar and sets the active button
+function loadHeader(pageName) {
+	$("#header").load("header.html", function() {
+		switchHeader(pageName);
+	});
+}
+
+// Changes the active navbar button
+function switchHeader(pageName) {
+	$(".nav-button").each(function() {
+		if ($(this).text() == pageName) {
+			$(this).attr("class", "nav-button-on");
+		}
+	});
+}
+
+$(document).ready(function() {
+	loadHeader("Home");
+});
