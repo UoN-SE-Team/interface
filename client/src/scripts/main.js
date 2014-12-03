d59f41eaa102f509d3af47e4ca010ac166955f87
Andy Palmer <alpsquid@gmail.com>
2014-12-03 00:13:56 +0000

    Added card button components, created main layout including responsive card positioning. Base styling of components done.

--- client/src/scripts/main.js
+++ client/src/scripts/main.js
@@ -1,9 +1,13 @@
 
 // Loads page content, sets the title and changes the active navbar button
-function loadPage(id, title, file) {
+function loadPage(id, title, file, callback) {
 	// In the final implementation, we'll be using a full templating framework so SEO won't be a problem
 	if (title != document.title) {
-		$(id).load(file);
+		$(id).load(file, function() {
+			if (callback != undefined) {
+				callback();
+			}
+		});
 		document.title = title + " - " + document.title.split(" - ")[1];
 		switchHeader(title);
 	}
@@ -25,7 +29,14 @@ function switchHeader(pageName) {
 	});
 }
 
+function initMasonry() {
+	var $container = $('#content');
+	$container.masonry({
+		columnWidth: 20
+	});
+}
+
 $(document).ready(function() {
 	loadHeader("Home");
-	loadPage('#content', 'Home', 'pages/home.html');
+	loadPage('#content', 'Home', 'pages/home.html', initMasonry);
 });
