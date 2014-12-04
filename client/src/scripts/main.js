aa60aebb4dfe89a0742fc2aa000fe433bdc8caf0
Andy Palmer <alpsquid@gmail.com>
2014-12-04 19:06:03 +0000

    Overlay done, just gallery functionality to add

--- client/src/scripts/main.js
+++ client/src/scripts/main.js
@@ -1,3 +1,8 @@
+/* 
+	@author Andy palmer
+	I've come up with a thrown together pseudo templating framework whcih emulates the functionality of a full Java 	framework (like Spring) which we'll use for the actual implementation.
+*/
+
 
 // Loads page content, sets the title and changes the active navbar button
 function loadPage(id, title, file, callback) {
@@ -30,10 +35,47 @@ function switchHeader(pageName) {
 	});
 }
 
-function showOverlay() {
+function showOverlay(title, actions /* [title, url ...] */, images /* [path ...] */, contentHeader, content, contentAltHeader, contentAlt) {
+	// title: 			 Overlay title
+	// actions: 		 Quick links on the right of the title (array: [title, url ...]
+	// images: 		 	 Gallery images (array: [path ...])
+	// contentHeader: 	 Title of left content section
+	// content: 		 HTML string of left content section
+	// contentAltHeader: Title of right content section
+	// contentAlt:		 HTML string of right content section 
 	$("#overlay-container").width("100%");
 	$("#overlay-container").height("100%");
 	$("#overlay-container").load("overlay.html", function() {
+		// title
+		$("#overlay #overlay-title").html(title);
+		// actions
+		if (actions != null && actions != undefined) {
+			for (var i=0; i < actions.length; i+=2) {
+				$("#overlay #overlay-actions").append(
+					"<a class='overlay-action' href='"+actions[i+1]+"'>"+actions[i]+"</a>"
+				);
+			}
+		}
+		// images
+		if (images != null && images != undefined) {
+			for (var i=0; i < images.length; i++) {
+				$("#overlay #overlay-gallery #gallery-images-container").append(
+					"<img src='"+images[i]+"'/>"
+				);
+			}
+		} else {
+			$("#overlay #overlay-gallery").width("0");
+			$("#overlay #overlay-gallery").height("0");
+			$("#overlay #overlay-gallery").css("display", "none");
+		}
+		// content header
+		$("#overlay #overlay-content #panel-1 .content-title").html(contentHeader);
+		// content
+		$("#overlay #overlay-content #panel-1 .content").html(jQuery.parseHTML(content));
+		// content Alt header
+		$("#overlay #overlay-content #panel-2 .content-title").html(contentAltHeader);
+		// content Alt
+		$("#overlay #overlay-content #panel-2 .content").html(jQuery.parseHTML(contentAlt));
 		
 	});
 }
@@ -65,6 +107,16 @@ $(document).ready(function() {
 	loadHeader("Home");
 	loadPage('#content', 'Home', 'pages/home.html', initMasonry);
 	wrapContent();
+	showOverlay('Example Title', 
+	                                                ['an action', 'http://google.com', 'an action 2', 'http://google.com'], 
+	                                                ['pages/assets/example_gallery/image1.png', 
+	                                                 'pages/assets/example_gallery/image2.png', 
+	                                                 'pages/assets/example_gallery/image3.png'], 
+	                                                'test content head', 
+	                                                'test content <a>with link</a>', 
+	                                                'test alt content head', 
+	                                                'test alt content <a>with link</a>'
+	                                               );
 });
 
 $(window).resize(function() {
